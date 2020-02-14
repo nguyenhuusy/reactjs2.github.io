c7b9e74c3802c8a0de21997fcb71182778132e8f
nguyenhuusy <52096108+nguyenhuusy@users.noreply.github.com>
2020-02-14 13:41:41 +0700

    Updates

---
+++ precache-manifest.ab6b833cc6558ce4dd2d5cb2b2127c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5c9a4e8790864bda183011ddbdc420",
+    "url": "/reactjs2.github.io/index.html"
+  },
+  {
+    "revision": "65af11a1aa48929cd389",
+    "url": "/reactjs2.github.io/static/css/main.7ca83cf3.chunk.css"
+  },
+  {
+    "revision": "7d5a29fa926205cd858d",
+    "url": "/reactjs2.github.io/static/js/2.60e80250.chunk.js"
+  },
+  {
+    "revision": "9fcbc669d68edcb506b730c9ae8a8e55",
+    "url": "/reactjs2.github.io/static/js/2.60e80250.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65af11a1aa48929cd389",
+    "url": "/reactjs2.github.io/static/js/main.bb8ff0bb.chunk.js"
+  },
+  {
+    "revision": "5022ee70a15e1fed3de3",
+    "url": "/reactjs2.github.io/static/js/runtime-main.cf6bc731.js"
+  }
+]);
